85bfc8e1de58d4bde939cdcd6ecfe3afbc5753fb
Khanh Nguyen <khanhcoluy@gmail.com>
2023-11-09 23:24:15 +0700

    - Make sidebar resizable - add sidebar library expand button

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -1,11 +1,41 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+
 import Library from './sidebarLibrary/Library';
 import Navigation from './sidebarNavigation/SidebarNavigation';
 
 const Sidebar = () => {
+  const [isResizing, setIsResizing] = useState(false);
+
+  useEffect(() => {
+    if (isResizing) {
+      const onMouseMove = (e: MouseEvent) => {
+        const sidebar = document.querySelector('.sidebar') as HTMLDivElement;
+        sidebar.style.width = `${e.clientX}px`;
+      };
+
+      const onMouseUp = () => {
+        setIsResizing(false);
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+      };
+
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+    }
+  }, [isResizing]);
+
   return (
-    <div className="flex flex-col gap-y-2 w-[450px] h-screen">
-      <Navigation />
-      <Library />
+    <div className="relative sidebar w-[450px] h-screen">
+      <div className="flex flex-col gap-y-2 h-full">
+        <Navigation />
+        <Library />
+      </div>
+      <div
+        className="resize-bar absolute right-0 top-0 h-full hover:border-dotted w-2 cursor-col-resize"
+        onMouseDown={() => setIsResizing(true)}
+      ></div>
     </div>
   );
 };
--- components/sidebar/sidebarLibrary/header/LibraryHeader.tsx
+++ components/sidebar/sidebarLibrary/header/LibraryHeader.tsx
@@ -1,17 +1,22 @@
 import { BiLibrary } from 'react-icons/bi';
-import { AiOutlinePlus } from 'react-icons/ai';
+import { AiOutlineArrowRight, AiOutlinePlus } from 'react-icons/ai';
 
 const LibraryHeader = () => {
   return (
-    <div className="pt-2 text-white">
+    <div className="pt-2 text-white ease-in-out duration-300">
       <header className="text-secondary-color flex justify-between items-center px-4 py-2 gap-3 w-full">
-        <div className="flex gap-3 cursor-pointer hover:text-primary-color ease-in-out duration-300">
+        <div className="flex gap-3 cursor-pointer hover:text-primary-color ">
           <BiLibrary size={25} />
           <p>Library</p>
         </div>
-        <button className="flex justify-center items-center hover:text-primary-color hover:bg-slate-800 w-7 h-7 ease-in-out duration-300 rounded-full">
-          <AiOutlinePlus className="text-[1.125rem]" />
-        </button>
+        <div className="flex gap-1">
+          <button className="flex justify-center items-center hover:text-primary-color hover:bg-slate-800 w-7 h-7 rounded-full">
+            <AiOutlinePlus className="text-[1.125rem]" />
+          </button>
+          <button className="hidden xl:flex justify-center items-center hover:text-primary-color hover:bg-slate-800 w-7 h-7 rounded-full">
+            <AiOutlineArrowRight className="text-[1.125rem]" />
+          </button>
+        </div>
       </header>
       <div>FilterGenreBox</div>
     </div>
